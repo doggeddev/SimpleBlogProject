21d3506a5998911e01b291d0b11a13fa4948ed37
Richard Drexel <richdrexel@gmail.com>
2017-05-14 23:48:41 -0400

    Added Admin Area, Basic Routing

--- SimpleBlogProject/App_Start/RouteConfig.cs
+++ SimpleBlogProject/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using SimpleBlogProject.Controllers;  //Had to import due to Admin Area PostsController conflicting with PostsController
 
 namespace SimpleBlogProject
 {
@@ -11,9 +12,15 @@ namespace SimpleBlogProject
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            var namespaces = new[] {typeof(PostsController).Namespace};
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute("Home", "", new {controller = "Posts", action = "Index"});
+            /*this matches the URL: ../login */ 
+            routes.MapRoute("Login", "login", new {controller = "Auth", action = "Login"}, namespaces);
+
+            
+            routes.MapRoute("Home", "", new {controller = "Posts", action = "Index"}, namespaces);
 
         }
     }
